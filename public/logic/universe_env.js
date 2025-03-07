e227e0c9112cd4e878c4069b0c86cbf8cfe310e2
aaron-chase <140291392+aaron-chase@users.noreply.github.com>
2025-03-06 19:16:18 -0500

    Updated string to morph logic

--- public/logic/universe_env.js
+++ public/logic/universe_env.js
@@ -1,17 +1,5 @@
 AFRAME.registerComponent('universe-env', {
-
-    schema: {
-        numMorphs: { type: 'int', default: 500 },
-        range: { type: 'int', default: 50 },
-    },
-
     init: function () {
-        for (let i = 0; i < this.data.numMorphs; i++) {
-            const morph = document.createElement('a-entity');
-            morph.setAttribute('morph', "objectPosRange: " + this.data.range);
-            this.el.appendChild(morph);
-        }
-
         const floor = document.createElement('a-plane');
         floor.setAttribute('id', 'floor');
         floor.setAttribute('width', 1000);
