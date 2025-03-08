aa3381274209d87d6e242e2bc490b85d8b205d99
aaron-chase <140291392+aaron-chase@users.noreply.github.com>
2025-03-07 19:27:00 -0500

    New files

---
+++ public/logic/global.js
@@ -0,0 +1,9 @@
+class GlobalTracker {
+    static currentStringId = "color";
+    static haloRadius = 5;
+    static numMorphs = 25;
+}
+
+const stringPurposeMap = { "color": "purple", "geometry": "blue", "size": "green", "position": "black", "internalRotationAngle": "orange", "emissiveIntensity": "yellow" };
+
+const avatarColorMap = { "blue": "orange", "yellow": "purple", "red": "cyan", "green": "magenta" };
