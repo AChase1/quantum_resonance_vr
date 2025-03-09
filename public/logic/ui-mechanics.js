b9b82a34ee6a177ffd6e867e315383140ef8d7e9
aaron-chase <140291392+aaron-chase@users.noreply.github.com>
2025-03-09 19:58:52 -0400

    Added instructions ui

---
+++ public/logic/ui-mechanics.js
@@ -0,0 +1,21 @@
+AFRAME.registerComponent("ui-mechanics", {
+    init: function () {
+        this.showControls = false;
+        document.getElementById("controls-menu").style.display = "none";
+        document.getElementById("controls-label").style.display = "block";
+
+        document.addEventListener("keydown", (event) => {
+            if (event.key === "i") {
+                if (this.showControls) {
+                    document.getElementById("controls-menu").style.display = "none";
+                    document.getElementById("controls-label").style.display = "block";
+                    this.showControls = false;
+                } else {
+                    document.getElementById("controls-menu").style.display = "block";
+                    document.getElementById("controls-label").style.display = "none";
+                    this.showControls = true;
+                }
+            }
+        });
+    },
+});
