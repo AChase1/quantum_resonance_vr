930b4535eb412373bceea77b6ca6dc9f7b29ac84
aaron-chase <140291392+aaron-chase@users.noreply.github.com>
2025-03-09 23:23:14 -0400

    Fixed ui elements

---
+++ public/logic/start-experience.js
@@ -0,0 +1,7 @@
+AFRAME.registerComponent("start-experience", {
+    init: function () {
+        // force user to click "Enter Experience" button
+        document.querySelector("#user-gesture-button").style.display =
+            "block";
+    },
+});
